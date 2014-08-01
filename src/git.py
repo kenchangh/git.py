477172b1480ba2688ea746e6c5686614e0a51876
Maverick Chan <guanhao3797@gmail.com>
2014-08-01 11:20:33 +0800

    Added more features
    Features added:
     - Git commit
     - Get remotes list

--- src/git.py
+++ src/git.py
@@ -35,20 +35,30 @@ class Repo(object):
     def __init__(self, repo=os.getcwd()):
         self.repo = repo
         self.is_bare = self.check_is_bare()
+        self.remotes = self.get_remotes()
         shell.call('cd {repo}'.format(repo = repo))
 
     def init(self, options=''):
         options = parse_options(options)
         shell.call('git init {options}'.format(options = options))
 
-    def check_is_bare():
+    def get_remotes(self):
+        remotes_dir = path.join(self.repo,
+                                '.git',
+                                'logs',
+                                'refs',
+                                'remotes')
+        return os.listdir(remotes_dir)
+
+    def check_is_bare(self):
         git_dir = path.join(self.repo, '.git')
-        return path.exists(git_dir)
+        # Negate exists, is_bare means it does not exist.
+        return not path.exists(git_dir)
 
     def add(self, files=[], options=''):
         options = parse_options(options)
         if files == []:
-            shell.call('git add --all {options}:/'.format(
+            shell.call('git add --all :/ {options}'.format(
                        options = options))
         elif type(files) is list:
             files = ' '.join(files)
@@ -56,4 +66,18 @@ class Repo(object):
         else:
             raise TypeError('Files has to be list, not {0}'.format(
                             type_in_str(files)))
+
+    def commit(self, files=[], message='', options=''):
+        options = parse_options(options)
+        message = '-m {0} '.format(message)
+        if files == []:
+            shell.call('git commit {options}{message}'.format(
+                       options = options, message = message))
+        elif type(files) is list:
+            files = ' '.join(files)
+            shell.call('git commit {options}{message}'.format(options = options) + files)
+        else:
+            raise TypeError('Files has to be in a list, not {0}'.format(
+                            type_in_str(files)))
+
 
